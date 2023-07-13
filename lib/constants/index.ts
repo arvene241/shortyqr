3248191595927698c701049deb0058bd05fc11b1
arvene <arvene241@gmail.com>
2023-07-13 14:56:51 +0800

    tweak

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,4 +1,4 @@
 export const NavLinks = [
-  { href: '/login', key: 'Login', text: 'Log in', button: false},
-  { href: '/signup', key: 'Signup', text: 'Sign up', button: true},
+  { href: '/', key: 'Login', text: 'Log in', button: false},
+  { href: '/', key: 'Signup', text: 'Sign up', button: true},
 ]
